3fbdc65f83594490085686c5b9f2056f98dca077
smail bensaad <amsmailbensaad@gmail.com>
2023-07-20 16:30:53 +0200

    finalversion

---
+++ index.js
@@ -0,0 +1,22 @@
+const bookmark = document.querySelector('[data-js="card-bookmark"]');
+const answerButton = document.querySelector('[data-js="answer-button"]');
+const answerCard = document.querySelector('[data-js="card-answer"]');
+answerCard.style.display = "none";
+bookmark.addEventListener("click", (event) => {
+bookmark.classList.toggle("hidden");
+});
+answerButton.addEventListener("click", (event) => {
+if (answerCard.style.display === "none") {
+    answerCard.style.display = "block";
+    answerButton.innerHTML = "Hide Answer";
+} else {
+    answerCard.style.display = "none";
+    answerButton.innerHTML = "Show Answer";
+}
+});
+
+
+
+
+
+
